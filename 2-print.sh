cb50ddd9c18614c830a661304f97a2f599045f48
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-05-29 15:10:33 +0530

    added 2-print.sh file

---
+++ 2-print.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#echo and printf commands can be used to print some message on screen
+#however we use echo command here,because it is simple and yet effective to all the need
+
+#syntax: echo input message
+
+echo this is sathya
+
